ef2e8c188f99bb5e82bb78a34451902f3e3ff3bf
Melis Peker <melis.peker@hisarschool.k12.tr>
2024-12-15 23:18:23 +0300

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Melis Peker on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
